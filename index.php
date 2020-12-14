70db9c3a35d2f70e38b5f0b04cd35b7da578933e
Villiss <lukas.vilim@outlook.com>
2020-12-14 09:35:26 +0100

    opraveny formular - iba jemne syntakticke zmeny

--- index.php
+++ index.php
@@ -53,10 +53,9 @@ if (isset($_SESSION['username'])) {
 ?>
 	<form method="post">
 		<p><label for="username">Prihlasovacie meno</label></p>
-		<input name="username" type="text" size="30" maxlength="30" id="username" placeholder="username" value="<?php if (isset($_POST["username"])) echo $_POST["username"]; ?>" ><br>
+		<input name="username" type="text" size="30" maxlength="30" id="username" placeholder="username" value="<?php if (isset($_POST["username"])) echo $_POST["username"]; ?>" />
 		<p><label for="password">Prihlasovacie heslo</label> </p>
-		<input name="password" type="password" size="30" placeholder="password" maxlength="30" id="password"> 
-		</>
+		<input name="password" type="password" size="30" placeholder="password" maxlength="30" id="password"/> 
 		<p>
 			<input name="login" type="submit" id="login" value="Log in">
 		</p>
