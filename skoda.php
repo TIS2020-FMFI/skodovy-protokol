e28206bf1d94413b550d176cca565cb2f23103d2
Villiss <lukas.vilim@outlook.com>
2021-01-03 12:13:47 +0100

    pridane vim, model a color, oseknuty background

--- skoda.php
+++ skoda.php
@@ -19,11 +19,32 @@
 </style>
 <?php } else { ?>
 <style>
+	input[type='text']{
+		position: absolute;
+		font-size:40px;
+		text-align:center;
+	}
+
+	#vin {
+	top: 420px;
+	height: 72px;
+    left: 304px;
+    width: 884px;
+	}
+
+
 	#model {
-		position: absolute;  
-		top: 250px; 		
-		left: 160px; 
-		width: 700px; 	
+    top: 502px;
+    left: 304px;
+    width: 884px;
+	height: 72px;
+	
+	}
+	#color {
+    top: 585px;
+    left: 304px;
+    width: 884px;
+	height: 59px;
 	}
 </style>
 <?php } ?>
@@ -38,8 +59,8 @@
   <div id="o-back">
     <form method="post">			
 	<input name="model" type="text" id="model" value="<?php if (isset($_POST["model"])) echo $_POST["model"]; ?>" size="20" maxlength="20">
-	
-
+	<input name="color" type="text" id="color" value="<?php if (isset($_POST["color"])) echo $_POST["color"]; ?>" size="20" maxlength="20">
+	<input name="vin" type="text" id="vin"  value="<?php if (isset($_POST["vin"])) echo  $_POST["vin"]; ?>" size="20" minlength="17" maxlength="17">
 	<br>
 	
 <p><input name="submit" type="submit" id="submit" value="Dowland PDF"></p>
