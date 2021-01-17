4a297eafb85313f964e63496b57495cfc15ece6c
Villiss <lukas.vilim@outlook.com>
2021-01-17 20:43:22 +0100

    duplicita header

--- protocol.php
+++ protocol.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Protokol</title>
-</head>
-<body>
-
 <?php
 
 // Include autoloader 
@@ -96,5 +87,3 @@ else if (isset($_POST["next"])) {
 ?>
 	
 </section>
-</body>
-</html>
