53a9e8c34729afa1e122cffa17405071d99bd24e
MartinKristak <kristak.martin44@gmail.com>
2021-01-29 18:15:19 +0100

    pridany podpis

--- skoda.php
+++ skoda.php
@@ -523,5 +523,5 @@
 	 } ?>
 	<br>	
 <p><input name="submit" type="submit" id="submit" value="Download PDF"></p>
-<p><input name="next" type="submit" id="next" value="Next"></p>
+<p><input name="next" type="submit" id="next" value="Next" <?php if (isset($_POST['submit'])) echo 'style="display: none;"'; ?>></p>
 </form>
