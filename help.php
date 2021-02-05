99fbd8b3601413b7e4ff3ad4dd01bcf668b51fb4
Villiss <lukas.vilim@outlook.com>
2021-02-05 20:15:51 +0100

    jaguar preklad hotovy

--- help.php
+++ help.php
@@ -1,6 +1,304 @@
 <?php 
 if (isset($_GET['type']) && strcmp($_GET['type'], "jaguar") == 0) {
-    echo '<img src="pictures/jaguar-help.jpg">'; 
+    echo 
+    '<table>
+        <tr>
+            <th>Číslo dielu.</th>
+            <th>Názov poškodeného dielu.</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Predný nárazník - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Predný spojler - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Predný blatník - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Predný podblatník - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Predný disk / puklica - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Predná pneumatika - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Vonkajšie spätné zrkadlo - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Interiér.</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Predné dvere - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Predný prah dverí - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Zadné dvere - ľavá strana (a stredový stĺpik ale len 4 dverové auto) .</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Zadný prah dverí - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Zadný podblatník - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Zadný disk / puklica - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>Zadná pneumatika - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>16</td>
+            <td>Zadný blatník (celý panel) - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>Zadný nárazník - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>18</td>
+            <td>Zadný deflektor - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>Zadný deflector.</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>Zadný blatník.</td>
+        </tr>
+        <tr>
+            <td>21</td>
+            <td>Zadný panel (priestor okolo EČV).</td>
+        </tr>
+        <tr>
+            <td>22</td>
+            <td>Zadné kufrové svetlá - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>23</td>
+            <td>Zadné kufrové svetlá - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>24</td>
+            <td>Zadné sklápacie okno / kufor.</td>
+        </tr>
+        <tr>
+            <td>25</td>
+            <td>Zadné sklo.</td>
+        </tr>
+        <tr>
+            <td>26</td>
+            <td>Zadný deflector - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>27</td>
+            <td>Zadný nárazník - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>28</td>
+            <td>Zadný blatník (celý panel) - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>29</td>
+            <td>Veko palivovej nádrže.</td>
+        </tr>
+        <tr>
+            <td>30</td>
+            <td>Zadný podblatník - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>31</td>
+            <td>Zadný disk / puklica - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>32</td>
+            <td>Zadná pneumatika - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>33</td>
+            <td>Zadné dvere - pravá strana (a stredový stĺpik ale len 4 dverové auto) .</td>
+        </tr>
+        <tr>
+            <td>34</td>
+            <td>Zadný prah dverí - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>35</td>
+            <td>Predné dvere - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>36</td>
+            <td>Predný prah dverí - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>37</td>
+            <td>Vonkajšie spätné zrkadlo - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>38</td>
+            <td>Predný podblatník - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>39</td>
+            <td>Predný blatník - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>40</td>
+            <td>Predný disk a puklica - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>41</td>
+            <td>Predná pneumatika - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>42</td>
+            <td>Predný blatník - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>43</td>
+            <td>Predný deflektor - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>44</td>
+            <td>Predný deflektor.</td>
+        </tr>
+        <tr>
+            <td>45</td>
+            <td>Predný nárazník.</td>
+        </tr>
+        <tr>
+            <td>46</td>
+            <td>Svetlá a smerovky - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>47</td>
+            <td>Svetlomety a smerovky - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>48</td>
+            <td>Dodatočné svetlá (ak sú namontované).</td>
+        </tr>
+        <tr>
+            <td>49</td>
+            <td>Mriežka.</td>
+        </tr>
+        <tr>
+            <td>50</td>
+            <td>Kapota.</td>
+        </tr>
+        <tr>
+            <td>51</td>
+            <td>Kryt motora.</td>
+        </tr>
+        <tr>
+            <td>52</td>
+            <td>Stierače.</td>
+        </tr>
+        <tr>
+            <td>53</td>
+            <td>Čelné sklo a A stĺpiky.</td>
+        </tr>
+        <tr>
+            <td>54</td>
+            <td>Strecha (a strešné okno ak je namontované).</td>
+        </tr>
+        <tr>
+            <td>55</td>
+            <td>Rezervné koleso.</td>
+        </tr>
+        <tr>
+            <td>56</td>
+            <td>Náradie na opravu kolesa.</td>
+        </tr>
+        <tr>
+            <td>57</td>
+            <td>Rádio.</td>
+        </tr>
+        <tr>
+            <td>58</td>
+            <td>Batéria.</td>
+        </tr>
+        <tr>
+            <td>59</td>
+            <td>Kľúče.</td>
+        </tr>
+        <tr>
+            <td>60</td>
+            <td>Spodok vozidla a výfuk.</td>
+        </tr>
+        <tr>
+            <td>61</td>
+            <td>Rôzne / iné.</td>
+        </tr>
+        <tr>
+            <td>62</td>
+            <td>Predné okno - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>63</td>
+            <td>Zadné okno - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>64</td>
+            <td>Zadné okno - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>65</td>
+            <td><span>Predné okno - pravá strana (vodičové) - pravostranné riadenie.</span></td>
+        </tr>
+        <tr>
+            <td>66</td>
+            <td>Predné spolujazdcové okno - pravostranné riadenie.</td>
+        </tr>
+        <tr>
+            <td>67</td>
+            <td>Zadné bočné okno - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>68</td>
+            <td>Zadné bočné strešné okno - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>69</td>
+            <td>Zadné bočné strešné okno - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>70</td>
+            <td>Zadná hmlovka - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>71</td>
+            <td>Zadná hmlovka - pravá strana.</td>
+        </tr>
+        <tr>
+            <td>72</td>
+            <td>Predná hmlovka - ľavá strana.</td>
+        </tr>
+        <tr>
+            <td>73</td>
+            <td>Predná hmlovka - pravá strana.</td>
+        </tr>
+    </table>';
 }    
 else if (isset($_GET['type']) && strcmp($_GET['type'], "opel") == 0) {
     echo '<img src="pictures/opel-help.jpg">'; 
@@ -16,3 +314,24 @@ else if (isset($_GET['type']) && strcmp($_GET['type'], "skoda") == 0) {
 }
 
 ?>
+
+<style>
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: center;
+  padding: 8px;
+  font-size: 24px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
+</style>
