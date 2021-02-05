a66e8e86fdbb4cd7e0b1d6fb7a9be298f8d3c358
qwerty7767 <claudia.b772@gmail.com>
2021-02-05 21:49:14 +0100

    preklad opel protokol

--- help.php
+++ help.php
@@ -301,7 +301,567 @@ if (isset($_GET['type']) && strcmp($_GET['type'], "jaguar") == 0) {
     </table>';
 }    
 else if (isset($_GET['type']) && strcmp($_GET['type'], "opel") == 0) {
-    echo '<img src="pictures/opel-help.jpg">'; 
+    echo 
+    '<table>
+        <tr>
+            <th>Area</th>
+            <th>Popis</th>
+            <th>Pozícia</th>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Ľavé zadné dvere</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Ľavé predné dvere</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Ľavé zadné dvere</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>14</td>
+            <td>Ľavý predný blatník</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>15</td>
+            <td>Ľavá stena karosérie</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>30</td>
+            <td>Vonkajšie spätné zrkadlo ľavé</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>35</td>
+            <td>Ľavý prah dverí</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>38</td>
+            <td>Ľavá ochranná lišta</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>70</td>
+            <td>B-stĺpik ľavý</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>72</td>
+            <td>A-stĺpik ľavý</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>74</td>
+            <td>C-stĺpik ľavý</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>75</td>
+            <td>Plech v kabíne vonkajší, vľavo</td>
+            <td>Vľavo</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Predný nárazník</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>ochranná lišta predného nárazníka</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>20</td>
+            <td>Čelné sklo</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>22</td>
+            <td>Predná mriežka</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>23</td>
+            <td>Zadný nárazník - ľavá strana.</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>24</td>
+            <td>Predné svetlo, smerovky</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>25</td>
+            <td>Hmlovky, denné svetlá</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>27</td>
+            <td>Kapota</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>42</td>
+            <td>zásterka predná</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>59</td>
+            <td>Predné stierače</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>80</td>
+            <td>Plastový kryt medzi čelným oknom a vekom motora</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>92</td>
+            <td>Priestor prednej podznačky</td>
+            <td>Vpredu</td>
+        </tr>
+        <tr>
+            <td>18</td>
+            <td>Predné rohože</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>19</td>
+            <td>Zadné rohože</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>26</td>
+            <td>Poťah stropu v interiéri</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>28</td>
+            <td>Kľúče</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>29</td>
+            <td>Vnútorné zrkadlo</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>33</td>
+            <td>Rádio</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>48</td>
+            <td>Vnútorné obloženie ľavých predných dverí</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>50</td>
+            <td>Vnútorné obloženie pravých predných dverí</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>55</td>
+            <td>Kufor, nakladací priestor</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>58</td>
+            <td>Reproduktory</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>61</td>
+            <td>vnútorný stredový box/odkladací priestor</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>66</td>
+            <td>Palubná doska</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>67</td>
+            <td>Zapaľovač</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>68</td>
+            <td>Koberec predný</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>78</td>
+            <td>Predĺženie bočnej steny, vľavo vzadu</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>79</td>
+            <td>Predĺženie bočnej steny, vpravo vzadu</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>85</td>
+            <td>Telefón</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>94</td>
+            <td>Ľavé predné sedadlo</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>95</td>
+            <td>Pravé predné sedadlo</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>96</td>
+            <td>Zadné sedadlo</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>97</td>
+            <td>Zadný koberec</td>
+            <td>Interiér</td>
+        </tr>
+        <tr>
+            <td>98</td>
+            <td>Interiér - ostatné</td>
+            <td>Interiér</td>
+        </tr>
+    </table>
+    
+    <br>
+
+    <table>
+        <tr>
+            <th>Area</th>
+            <th>Popis</th>
+            <th>Pozícia</th>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Pravé piate dvere</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Posuvné pravé dvere</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>12</td>
+            <td>Pravé predné dvere</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>13</td>
+            <td>Pravé zadné dvere</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>16</td>
+            <td>Pravý predný blatník</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>Pravý plech karosérie</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>31</td>
+            <td>Vonkajšie spätné zrkadlo pravé</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>36</td>
+            <td>Pravý prah dverí</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>39</td>
+            <td>Pravá ochranná lišta</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>69</td>
+            <td>B-stĺpik pravý</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>71</td>
+            <td>A-stĺpik pravý</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>73</td>
+            <td>C-stĺpik pravý</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>76</td>
+            <td>Plech v kabíne vonkajší, vpravo</td>
+            <td>Vpravo</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Anténa</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Zadný nárazník</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>ochranná lišta zadného nárazníka</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>21</td>
+            <td>Zadné sklo</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>34</td>
+            <td>Zadný koncový panel</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>40</td>
+            <td>Rezerva (Pneu a disk)</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>41</td>
+            <td>Lišta nad zadným nárazníkom</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>43</td>
+            <td>zadná zásterka</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>44</td>
+            <td>palivová nádrž</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>45</td>
+            <td>Zadné svetlá, smerovky</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>52</td>
+            <td>Veko kufra</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>57</td>
+            <td>Puklice</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>59</td>
+            <td>Zadné stierače</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>64</td>
+            <td>Zadný spojler</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>77</td>
+            <td>Plech v kabíne vzadu</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>81</td>
+            <td>Krytka nádrže</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>82</td>
+            <td>Lavý zadný blatník</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>83</td>
+            <td>Pravý zadný blatník</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>84</td>
+            <td>Sada náradia, zdvihák</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>92</td>
+            <td>Zadná podznačka</td>
+            <td>Vzadu</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Batéria</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>37</td>
+            <td>Strecha</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>47</td>
+            <td>Koleso/pneumatika</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>53</td>
+            <td>Strešné okno</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>54</td>
+            <td>ochrana-kryt motora</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>56</td>
+            <td>Sklápacia strecha (kabriolet)</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>62</td>
+            <td>Katalyzátor</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>63</td>
+            <td>Výstuže proti prevráteniu vozidla (kabriolet)</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>65</td>
+            <td>Strešný nosič/lyžiny</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>87</td>
+            <td>Predný plech</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>90</td>
+            <td>Rám</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>91</td>
+            <td>Výfuková sústava</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>93</td>
+            <td>Zavesenie kolies</td>
+            <td>Rôzne</td>
+        </tr>
+        <tr>
+            <td>99</td>
+            <td>Priestor motora</td>
+            <td>Rôzne</td>
+        </tr>
+    </table>
+
+    <br>
+
+
+    <table>
+    <tr>
+        <th>SC</th>
+        <th>Popis</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>škoda do 3cm</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>škoda od 3 do 10 cm</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>škoda od 10 do 20 cm</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>škoda od 20 do 30 cm</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>škoda nad 30 cm</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>vážna škoda</td>
+    </tr>
+    </table>
+
+    <br>
+
+    <table>
+    <tr>
+        <th>DC</th>
+        <th>Popis</th>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Zlomené</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Preliačené</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>obité (lak)</td>
+    </tr>
+    <tr>
+        <td>8</td>
+        <td>chýba</td>
+    </tr>
+    <tr>
+        <td>10</td>
+        <td>znečistený interiér</td>
+    </tr>
+    <tr>
+        <td>11</td>
+        <td>diera</td>
+    </tr>
+    <tr>
+        <td>12</td>
+        <td>škrabanec</td>
+    </tr>
+    <tr>
+        <td>21</td>
+        <td>sklo</td>
+    </tr>
+    </table>
+    
+    
+    '; 
 }
 else if (isset($_GET['type']) && strcmp($_GET['type'], "peugeot") == 0) {
     echo '<img src="pictures/peugeot-help.jpg">'; 
